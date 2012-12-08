4e121de04c11cc22542e2e66f0c8c27d8e1352bb
Matvey <motakuk@gmail.com>
2012-12-08 19:15:11 +0400

    Dobavil shabloni raboti s bd

--- dbCreator.php
+++ dbCreator.php
@@ -1,3 +1,8 @@
 <?php
-
+    $sql="SELECT DISTINCT recipient_id FROM rb_messages WHERE author_id='".mysql_real_escape_string(LoginInfo::$id)."'";//Запрос
+    $result=mysql_query($sql);//Отправляем в бд
+    if (!$result) {
+            echo 'Ошибка запроса: '.mysql_error();
+            exit;
+    }		
 ?>
--- index.php
+++ index.php
@@ -5,8 +5,19 @@
         <title></title>
     </head>
     <body>
-        <?php
-        // put your code here
+            <?php
+            require 'MySQLConnect.php';
+            
+            $sql="SELECT DISTINCT recipient_id FROM rb_messages WHERE author_id='".mysql_real_escape_string(LoginInfo::$id)."'";//Запрос
+            $result=mysql_query($sql);//Спрашиваем бд
+            if (!$result) {
+                    echo 'Ошибка запроса: '.mysql_error();
+                    exit;
+            }
+
+            while ($lineRecipientId=mysql_fetch_row($result)){
+                    $connectedUsersId[]=$lineRecipientId[0];//Выцепляем результат
+            }
         ?>
     </body>
 </html>
